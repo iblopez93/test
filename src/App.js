7f250356c37f09d29278d3fbefa787107c55416c
iblopez <iblopez@cienpet.cupet.cu>
2021-10-28 19:46:54 -0400

    Adding first component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className='container'>
+        <div class="row justify-content-md-center">
+          <h1 className='col-md-auto text-danger'>Aurous</h1>
+        </div>
+        <div class="row justify-content-md-center">
+          <form>
+            <div className='justify-content-md-center p-4 '>
+              <label for="user_login" class="col-md-auto form-label">User</label>
+              <input id='user_login' className='form-control' />
+            </div>
+            <div className='justify-content-md-center p-4'>
+              <label for="pass_login" class="form-label col-md-auto">Password</label>
+              <input id='pass_login' className='form-control' />
+            </div>
+            <div className='col-auto p-4'>
+              <input id='submit_login' type="submit" className='form-control col-md-auto btn btn-primary' />
+            </div>
+
+
+          </form>
+        </div>
+
+      </div>
+
+    </>
+  )
 }
 
 export default App;
+
