32b5e5b74c61af1a534e27c425dcd1f228c5fbe4
iblopez <iblopez@cienpet.cupet.cu>
2021-10-28 20:19:12 -0400

    Login Component Ended

--- src/App.js
+++ src/App.js
@@ -5,21 +5,21 @@ const App = () => {
   return (
     <>
       <div className='container'>
-        <div class="row justify-content-md-center">
+        <div className="row justify-content-md-center">
           <h1 className='col-md-auto text-danger'>Aurous</h1>
         </div>
-        <div class="row justify-content-md-center">
-          <form className='col-md-auto'>
+        <div id="form" className="row justify-content-md-center">
+          <form className='col-md-auto shadow p-3 mb-5 bg-body rounded'>
             <div className='justify-content-md-center p-4 '>
-              <label for="user_login" class="col-md-auto form-label">User</label>
+              <label htmlFor="user_login" className="col-md-auto form-label">User</label>
               <input id='user_login' className='form-control' />
             </div>
             <div className='justify-content-md-center p-4'>
-              <label for="pass_login" class="form-label col-md-auto">Password</label>
-              <input id='pass_login' className='form-control' />
+              <label htmlFor="pass_login" className="form-label col-md-auto">Password</label>
+              <input id='pass_login' type='password' className='form-control ' />
             </div>
             <div className='col-auto p-4'>
-              <input id='submit_login' type="submit" className='form-control col-md-auto btn btn-primary' />
+              <input id='submit_login' type="submit" className='form-control col-md-auto btn btn-primary shadow rounded align-center' />
             </div>
 
 
